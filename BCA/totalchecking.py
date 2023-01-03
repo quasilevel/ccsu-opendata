673d94dd41ca90107d1e240ce50b3c957a6389b0
Deepanshu_PI <adeepanshu7@gmail.com>
2023-01-03 22:23:11 +0530

    change variable location,change variable name and use os module for path

--- BCA/totalchecking.py
+++ BCA/totalchecking.py
@@ -5,33 +5,33 @@ import  pandas as pd
 def checkTheSum75():
     all_files_name=glob.glob("*.json")
     currentPath=os.getcwd()
-    noFoYears=[]
     check75=0
     #loop for opening file 
     for l in all_files_name:
-        print("checking for Year : ",l)
-        sampleFile=pd.read_json(currentPath+"\\"+l)
+        noOfYears=[]
+        print("\nCode :",l)
+        sampleFile=pd.read_json(os.path.join(currentPath, l))
         
         #loop for counting no of year in each file
         for i in range(len(sampleFile['topics'])):
             for j in range(len(sampleFile['topics'][i]['weights'])):
-                noFoYears.append(sampleFile['topics'][i]['weights'][j]['year'])
-        noFoYears=list(set(noFoYears))
-        noFoYears.sort()
+                noOfYears.append(sampleFile['topics'][i]['weights'][j]['year'])
+        noOfYears=list(set(noOfYears))
+        noOfYears.sort()
 
-        for i in range(len(noFoYears)):
+        for i in range(len(noOfYears)):
             for j in range(len(sampleFile['topics'])):
                 for k in range(len(sampleFile['topics'][j]['weights'])):
-                    if sampleFile['topics'][j]['weights'][k]['year']==noFoYears[i]:
+                    if sampleFile['topics'][j]['weights'][k]['year']==noOfYears[i]:
                         check75=check75+sampleFile['topics'][j]['weights'][k]['weight']
             if check75 > 75:
-                print(noFoYears[i]," This year sum is more than 75 and the total is : ",check75)
+                print("Year ",noOfYears[i],": total is : ",check75,"expected 75")
             elif check75<75:
-                print(noFoYears[i]," This year sum is less than 75 and total is :",check75)
+                print("Year ",noOfYears[i],": total is : ",check75,"expected 75")
             elif check75==75:
-                print(noFoYears[i],"Pass in test and the total is :",check75)
+                print("Year ",noOfYears[i],": total is : ",check75,"expected 75")
             check75=0
-        noFoYears=[]
+        noOfYears=[]
 
 
 if __name__=="__main__":
